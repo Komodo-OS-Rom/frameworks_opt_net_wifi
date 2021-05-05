84b85c8f34a030a635a658f0ccaee96b3404a2cd
Kevin F. Haggerty <haggertk@lineageos.org>
2021-05-05 06:37:14 -0600

    Revert "Don't expose MAC address in IPv6 address"
    This reverts commit d6ecd26cff564827d5a1ec4ccef772ce40ad4021.
    
    Change-Id: Id214f97b07f7f644014fe77424ff5018bdb6288b
    Signed-off-by: wulan17 <galihgustip@gmail.com>

--- service/java/com/android/server/wifi/ClientModeImpl.java
+++ service/java/com/android/server/wifi/ClientModeImpl.java
@@ -5398,6 +5398,7 @@ public class ClientModeImpl extends StateMachine {
                             .withApfCapabilities(mWifiNative.getApfCapabilities(mInterfaceName))
                             .withNetwork(getCurrentNetwork())
                             .withDisplayName(currentConfig.SSID)
+                            .withRandomMacAddress()
                             .build();
             } else {
                 StaticIpConfiguration staticIpConfig = currentConfig.getStaticIpConfiguration();
